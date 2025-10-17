d415ef15e00d63fb2a5b9d5c6b6c1367b748f1f8
Daniel Saavedra <daniel.saavedra.fon@gmail.com>
2025-10-16 21:01:56 -0500

    Add manifest

--- src/main/java/co/edu/unisabana/cursoarquitectura/SaludoController.java
+++ src/main/java/co/edu/unisabana/cursoarquitectura/SaludoController.java
@@ -1,6 +1,7 @@
 package co.edu.unisabana.cursoarquitectura;
 
 import co.edu.unisabana.cursoarquitectura.solid.d.dominio.PagoProducto;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,10 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SaludoController {
 
+    @Value("${pod:xxx}")
+    private String nombrePod;
+
 
     @GetMapping(path = "/saludo")
     public String saludar() {
-        return "todo bien desde K8S";
+        return "todo bien desde K8S desde el pod " + nombrePod;
     }
 
 }
